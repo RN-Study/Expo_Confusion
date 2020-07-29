62e8d8021e2bcd498f9ddd589d9222cef3cdf74c
HaBV90 <35767439+HaBV90@users.noreply.github.com>
2020-07-29 23:45:29 +0700

    Navigation Part 3

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,10 +1,48 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, {Component, useState} from 'react';
+import {View, Text, ScrollView} from 'react-native';
+import {Card} from 'react-native-elements';
+import {DISHES} from "../shared/dishes";
+import {LEADERS} from "../shared/leaders";
+import {PROMOTIONS} from "../shared/promotions";
+
+export const RenderItem = (props) => {
+    const item = props.item;
+    // if (props.isLoading) {
+    //     return <Loading />;
+    // } else if (props.errorMessage) {
+    //     return (
+    //       <View>
+    //           <Text>{props.errorMessage}</Text>
+    //       </View>
+    //     );
+    // } else {
+    if (item !== null) {
+        return (
+          <Card
+            featuredTitle={item.name}
+            featuredSubtitle={item.designation}
+            // image={{uri: baseURL + item.image}}
+            image={require('../assets/images/uthappizza.png')}
+          >
+              <Text style={{margin: 10}}> {item.description} </Text>
+          </Card>
+        );
+    } else {
+        return <View />;
+    }
+    // }
+};
 
 const Home = (props) => {
-
+    const [dishes] = useState(DISHES);
+    const [leaders] = useState(LEADERS);
+    const [promotions] = useState(PROMOTIONS);
     return(
-      <View><Text>Home Component</Text></View>
+      <ScrollView>
+          <RenderItem item={dishes.filter((dish) => dish.featured)[0]}/>
+          <RenderItem item={promotions.filter((promo) => promo.featured)[0]}/>
+          <RenderItem item={leaders.filter((leader) => leader.featured)[0]}/>
+      </ScrollView>
     );
 }
 
